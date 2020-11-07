d768d43c83ea93331a353d60ac560eb0dd29946e
Koyashiro Kohaku <develop@koyashiro.net>
2020-11-08 02:27:14 +0900

    feat: add `get_current_dir` method

--- rust-pwd/src/main.rs
+++ rust-pwd/src/main.rs
@@ -1,7 +1,14 @@
 use std::env;
 
 fn main() -> std::io::Result<()> {
-    let current_dir = env::current_dir()?;
-    println!("{}", current_dir.display());
+    let current_dir = get_current_dir()?;
+    println!("{}", current_dir);
     Ok(())
+}
+
+fn get_current_dir() -> Result<String, std::io::Error> {
+    let current_dir = env::current_dir()?;
+    let os_string = current_dir.into_os_string();
+    let string = os_string.into_string().unwrap();
+    Ok(string)
 }
