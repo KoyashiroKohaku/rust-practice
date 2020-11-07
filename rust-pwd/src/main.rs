93b6cd74471bd88a9d4acd7d1415b51c8d892aec
Koyashiro Kohaku <develop@koyashiro.net>
2020-11-08 02:55:12 +0900

    feat: implement argument parsing

--- rust-pwd/src/main.rs
+++ rust-pwd/src/main.rs
@@ -1,9 +1,33 @@
 use std::env;
 
 fn main() -> std::io::Result<()> {
+    let mode = get_mode();
+
+    match mode {
+        Mode::Physical => println!("mode is pysical"),
+        Mode::Logical => println!("mode is logical"),
+    }
+
     let current_dir = get_current_dir()?;
     println!("{}", current_dir);
     Ok(())
+}
+
+enum Mode {
+    Physical,
+    Logical,
+}
+
+fn get_mode() -> Mode {
+    let args: Vec<String> = env::args().collect();
+    let mode: Mode = if args.contains(&"-P".to_string()) {
+        Mode::Physical
+    } else if args.contains(&"-L".to_string()) {
+        Mode::Logical
+    } else {
+        Mode::Physical
+    };
+    mode
 }
 
 fn get_current_dir() -> Result<String, std::io::Error> {
