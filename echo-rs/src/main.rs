dca574cc5f9ad09d1acf0ae31cb5b2775870d4f8
koyashiro <develop@koyashiro.net>
2021-04-17 11:28:04 +0900

    refactor: use `collect()`

--- echo-rs/src/main.rs
+++ echo-rs/src/main.rs
@@ -1,8 +1,5 @@
 fn main() {
-    let mut args: Vec<String> = Vec::new();
-    for arg in std::env::args().skip(1) {
-        args.push(arg);
-    }
+    let args: Vec<String> = std::env::args().skip(1).collect();
     let result = args.join(" ");
     println!("{}", result);
 }
