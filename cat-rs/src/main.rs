245e4b31abefc4975e844ddd1a57d0db025ca98d
koyashiro <develop@koyashiro.net>
2021-04-17 12:55:44 +0900

    fix: use `borrowing`

--- cat-rs/src/main.rs
+++ cat-rs/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
     for arg in args {
-        let result = std::fs::read_to_string(arg);
+        let result = std::fs::read_to_string(&arg);
         match result {
             Ok(content) => {
                 for line in content.lines() {
