f204b0c6ec877abfda5563934548c406439dd05a
koyashiro <develop@koyashiro.net>
2021-04-17 11:57:00 +0900

    refactor: use stderr

--- cat-rs/src/main.rs
+++ cat-rs/src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
     for arg in args {
-        let content = std::fs::read_to_string(arg).expect("error: `{}` is not exists!");
-        for line in content.lines() {
-            println!("{}", line);
-        }
+        let result = std::fs::read_to_string(arg);
+        match result {
+            Ok(content) => {
+                for line in content.lines() {
+                    println!("{}", line);
+                }
+            }
+            Err(error) => eprintln!("[cat-rs error]: {}", error),
+        };
     }
 }
