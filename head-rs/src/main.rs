c8ec66db38c921329755b0f89f13becd6b01e4f2
koyashiro <develop@koyashiro.net>
2021-04-22 01:31:42 +0900

    feat: add `structopt` and support `lines` option

--- head-rs/src/main.rs
+++ head-rs/src/main.rs
@@ -1,15 +1,20 @@
 use anyhow::Result;
 use std::fs::read_to_string;
-use std::{env::args, process::exit};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt(name = "path")]
+    path: String,
+
+    #[structopt(short = "n", long = "lines", value_name = "NUM", default_value = "10")]
+    lines: usize,
+}
 
 fn main() -> Result<()> {
-    let path = match args().nth(1) {
-        Some(path) => path,
-        None => exit(1),
-    };
-
-    let content = read_to_string(path)?;
-    for line in content.lines().take(10) {
+    let opt = Opt::from_args();
+    let content = read_to_string(opt.path)?;
+    for line in content.lines().take(opt.lines) {
         println!("{}", line);
     }
 
